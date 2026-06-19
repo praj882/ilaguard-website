a41a371287f19b453d7ce616d1aed93f02dc6ede
praj882 <rajprashant882@gmail.com>
2026-06-19 19:32:42 +0530

    Hero page fix without swiper

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,26 +1,49 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function HeroSlider() {
-  return (
-    <Swiper>
-      <SwiperSlide>
-        <img
-          src="/images/A_futuristic_innovation_lab.jpg"
-          alt="Slide 1"
-          className="w-full h-72 object-cover"
-        />
-      </SwiperSlide>
+  const images = [
+    "/images/A_futuristic_innovation_lab.jpg",
+    "/images/Young_engineering_students.jpg",
+    "/images/Smart_irrigation_system.jpg",
+    "/images/A_smart_water_tank_monitoring_system.jpg",
+    "/images/Embedded_systems_engineering_workspace.jpg",
+  ];
 
-      <SwiperSlide>
-        <img
-          src="/images/Young_engineering_students.jpg"
-          alt="Slide 2"
-          className="w-full h-72 object-cover"
-        />
-      </SwiperSlide>
-    </Swiper>
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % images.length);
+    }, 3500);
+
+    return () => clearInterval(timer);
+  }, [images.length]);
+
+  return (
+    <div className="relative w-full h-72 md:h-96 lg:h-[500px] overflow-hidden rounded-3xl shadow-2xl">
+      <Image
+        src={images[current]}
+        alt={`Slide ${current + 1}`}
+        fill
+        priority
+        className="object-cover transition-opacity duration-700"
+      />
+
+      {/* Dots */}
+      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrent(index)}
+            className={`w-3 h-3 rounded-full ${
+              current === index ? "bg-white" : "bg-white/50"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
